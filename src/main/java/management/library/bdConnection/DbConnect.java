80815c82761efd26a97c227b35f7e832823dad47
fanambynana <hei.fanambinana@gmail.com>
2023-11-19 12:36:50 +0300

    Set database connection

--- src/main/java/management/library/bdConnection/DbConnect.java
+++ src/main/java/management/library/bdConnection/DbConnect.java
@@ -1,4 +1,20 @@
 package management.library.bdConnection;
 
-public class CreateConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnect {
+    public Connection createConnection() {
+        try {
+            return DriverManager.getConnection(
+                    PsqlConf.URL,
+                    PsqlConf.USERNAME,
+                    PsqlConf.PASSWORD
+            );
+        } catch (SQLException exception) {
+            System.out.println("Connection error :\n" + exception.getMessage());
+            return null;
+        }
+    }
 }
