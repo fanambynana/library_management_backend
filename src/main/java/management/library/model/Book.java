f369824f37cf2daffaed2d5cab7ea718f9fa2021
fanambynana <hei.fanambinana@gmail.com>
2023-11-19 18:13:06 +0300

    make pageNumber to int

--- src/main/java/management/library/model/Book.java
+++ src/main/java/management/library/model/Book.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Book {
     private String id;
     private String bookName;
-    private String pageNumber;
+    private int pageNumber;
     private LocalDate releaseDate;
     private List<Topic> topic;
 }
