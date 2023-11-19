2cb9cb80c33059e37ec1b351a0ba05a688e9af29
fanambynana <hei.fanambinana@gmail.com>
2023-11-19 18:38:02 +0300

    implement findall in BookRepository

--- src/main/java/management/library/repository/BookRepository.java
+++ src/main/java/management/library/repository/BookRepository.java
@@ -2,12 +2,16 @@ package management.library.repository;
 
 import management.library.bdConnection.DbConnect;
 import management.library.model.Book;
+import management.library.model.Topic;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class BookRepository {
@@ -16,19 +20,24 @@ public class BookRepository {
 
 
     public List<Book> findAll() {
+        List<Book> bookList = new ArrayList<>();
         try {
-            List<Book> bookList = new ArrayList<>();
             String allRequest = "SELECT * FROM book";
             Statement allStatement = connection.createStatement();
             ResultSet allResultSet = allStatement.getResultSet();
 
             while (allResultSet.next()) {
                 bookList.add(new Book(
-
-                ))
+                        allResultSet.getString("id"),
+                        allResultSet.getString("book_name"),
+                        allResultSet.getInt("page_number"),
+                        (LocalDate) allResultSet.getObject("release_date"),
+                        new ArrayList<>(((Topic) allResultSet.getObject("topic")).ordinal())
+                ));
             }
         } catch (SQLException exception) {
             System.out.println("Error while finding all books :\n" + exception.getMessage());
         }
+        return bookList;
     }
 }
