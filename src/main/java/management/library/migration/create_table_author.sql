867ebecd72a30a83272a0891db83f4e7c3bb2869
fanambynana <hei.fanambinana@gmail.com>
2023-12-01 18:36:16 +0300

    fix: use snake case instead of camel case in SQL

--- src/main/java/management/library/migration/create_table_author.sql
+++ src/main/java/management/library/migration/create_table_author.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS author(
     id SERIAL PRIMARY KEY,
-    authorName VARCHAR(250) NOT NULL,
+    author_name VARCHAR(250) NOT NULL,
     sex CHAR(1) CHECK (sex = 'M' OR sex = 'F')
 );
 
-INSERT INTO author VALUES
+INSERT INTO author(author_name, sex) VALUES
 ('Agatha Christie', 'F'),
 ('J.R.R. Tolkien', 'M'),
 ('George R.R. Martin', 'M');
